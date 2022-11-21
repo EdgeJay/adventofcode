b3c822181285913019a202a38951ec6de94f0932
Wu Huijie <hjwu85@gmail.com>
2022-11-22 00:13:38 +0800

    solved 2022 day2 puzzle2

--- common/structs/submarine.go
+++ common/structs/submarine.go
@@ -3,8 +3,9 @@ package structs
 type Submarine struct {
 	Horizontal int
 	Depth      int
+	Aim        int
 }
 
 func NewSubmarine() *Submarine {
-	return &Submarine{0, 0}
+	return &Submarine{0, 0, 0}
 }
